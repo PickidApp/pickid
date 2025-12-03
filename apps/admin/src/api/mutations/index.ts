40358e388201da9e655441c5b4af88c54a6eb3c5
mingnana <alstjr9438@naver.com>
2025-12-03 22:10:26 +0900

    feat: prettier 적용

--- apps/admin/src/api/mutations/index.ts
+++ apps/admin/src/api/mutations/index.ts
@@ -1,2 +1,8 @@
 export { useUpdateFeedbackStatus } from './useUpdateFeedbackStatus';
+export { useUpsertTestMutation } from './useUpsertTest';
+export { useArchiveTestMutation } from './useArchiveTest';
+export { usePublishTestMutation } from './usePublishTest';
+export { useSyncQuestionsMutation } from './useSyncQuestions';
+export { useSyncResultsMutation } from './useSyncResults';
+export { useDuplicateTestMutation } from './useDuplicateTest';
 
